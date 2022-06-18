85dd1f6f126c6f45f5c21db2f834f60b5468f7be
Nitin Naik <107764213+nitinnaik13@users.noreply.github.com>
2022-06-18 12:29:56 -0400

    Update streamlit_app.py
    change list to fruit instead of index

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 #let's put a picklist here so they can pick the fruit they want to include
-streamlit.multiselect('Pick some fruit: ', list(my_fruit_list.index))
+streamlit.multiselect('Pick some fruit: ', list(my_fruit_list.fruit))
 
 #display the table on the page
 streamlit.dataframe(my_fruit_list)
