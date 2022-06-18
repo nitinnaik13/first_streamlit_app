363fae17db45df7cdd75adb7f0573bf11fc47244
Nitin Naik <107764213+nitinnaik13@users.noreply.github.com>
2022-06-18 17:30:09 -0400

    Update streamlit_app.py
    removed just writes response line

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,6 @@ streamlit.dataframe(fruits_to_show)
 #new section to display fruityvice api response
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json()) #just writes the data to the screen
 
 #take the json version of the reponse and normalize it
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
