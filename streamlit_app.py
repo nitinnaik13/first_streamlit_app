365750817c4c179811e6523dd35e4ee837d4eeb2
Nitin Naik <107764213+nitinnaik13@users.noreply.github.com>
2022-06-18 17:24:36 -0400

    Update streamlit_app.py
    fixed typo in response var

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,4 +24,4 @@ streamlit.dataframe(fruits_to_show)
 #new section to display fruityvice api response
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice.response)
+streamlit.text(fruityvice_response)
