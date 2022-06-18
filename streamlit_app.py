a63ef25757a271f40c9972e6a706da8479cccf6d
Nitin Naik <107764213+nitinnaik13@users.noreply.github.com>
2022-06-18 17:36:44 -0400

    Update streamlit_app.py
    corrected typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,7 @@ steamlit.write('The user entered', fruit_choice)
 
 #new section to display fruityvice api response
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ "kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
 
 #take the json version of the reponse and normalize it
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
