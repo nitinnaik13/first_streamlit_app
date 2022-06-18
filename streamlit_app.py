d3db45cfa8d31e62d6211f9a22b4793b35f06634
Nitin Naik <107764213+nitinnaik13@users.noreply.github.com>
2022-06-18 12:14:53 -0400

    Update streamlit_app.py
    Added header and text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
-import streamlit;
+import streamlit
 
 streamlit.title('My New Parents Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinatch & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
